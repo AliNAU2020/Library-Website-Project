08eaddd91735bed7e33208e4761c2ce003778ea4
AliNAU2020 <31328423+AliNAU2020@users.noreply.github.com>
2020-09-01 06:47:51 -0500

    Update index.php

--- index.php
+++ index.php
@@ -1,5 +1,6 @@
 <?php header ('Location: Web App Design Project/index.html') ; ?>
-<?php header ('Location: Web App Design Project/menu/Catalogue.html') ; ?>
+<?php header ('Location: Web App Design Project/menu/Catalouge.html') ; ?>
+<?php header ('Location: Web App Design Project/menu/Catalouge_2.html') ; ?>
 <?php header ('Location: Web App Design Project/menu/best_3_books.html') ; ?>
 <?php header ('Location: Web App Design Project/menu/about_us.html') ; ?>
 <?php header ('Location: Web App Design Project/menu/history.html') ; ?>
